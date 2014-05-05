b5f842d77326e811866ad182ded23b93407ec3a5
williamliusea <williamliu_sea@hotmail.com>
2014-05-05 11:58:13 -0700

    add question 2

---
+++ Question1/Question2/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    using System.IO;
+
+    public class Node
+    {
+        public int x;
+        public int y; 
+        public Node Next;
+    };
+
+    public class Cell : Node
+    {
+        public Cell(int x, int y, int value)
+        {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+            this.xMin = int.MaxValue/2;
+            this.yMin = this.xMin;
+            this.min = this.xMin;
+        }
+
+        public Node xNext;
+
+        public Node yNext;
+
+        public int xMin;
+
+        public int yMin;
+
+        public int min;
+
+        public int value;
+    }
+    public class path
+    {
+        public int difficulty; 
+        public Node Pathlink;
+    }
+    class Program
+    {
+        private static void Solve(int m, int n, int[,] matrix, Cell[,] t)
+        {
+            t[m - 1, n - 1] = new Cell(m - 1, n - 1, matrix[m - 1, n - 1]);
+            for (int i = 0; i < m - 1; i++)
+            {
+                var c = new Cell(i, n - 1, matrix[i, n - 1]);
+                c.xNext = t[m - 1, n - 1];
+                c.xMin = t[m - 1, n - 1].value;
+                c.Next = t[m - 1, n - 1];
+                t[i, n - 1] = c;
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                var c = new Cell(m - 1, i, matrix[m - 1, 1]);
+                c.Next = t[m - 1, n - 1];
+                c.yNext = t[m - 1, n - 1];
+                c.yMin = t[m - 1, n - 1].value;
+                t[m - 1, i] = c;
+            }
+
+            for (int i = m - 2; i >= 0; i--)
+            {
+                for (int j = n-2; j>=0; j--)
+                {
+                    t[i, j] = new Cell(i, j, matrix[i, j]);
+                    // y axis search
+                    if (t[i, j + 1].xMin+matrix[i,j+1]<t[i,j+1].yMin)
+                    {
+                        t[i, j].yNext = t[i, j + 1];
+                        t[i, j].yMin = t[i, j + 1].xMin + matrix[i, j + 1];
+                    }
+                    else
+                    {
+                        t[i, j].yNext = t[i, j + 1].yNext;
+                        t[i, j].yMin = t[i, j + 1].yMin;
+                    }
+
+                    // x axis search
+                    if (t[i + 1, j].yMin + matrix[i+1, j] < t[i + 1, j].xMin)
+                    {
+                        t[i, j].xNext = t[i+1, j ];
+                        t[i, j].xMin = t[i+1, j].yMin + matrix[i+1, j];
+                    }
+                    else
+                    {
+                        t[i, j].xNext = t[i+1, j].xNext;
+                        t[i, j].xMin = t[i+1, j ].xMin;
+                    }
+
+                    if (t[i, j].yMin < t[i, j].xMin)
+                    {
+                        t[i, j].Next = t[i, j].yNext;
+                        t[i, j].min = t[i, j].yMin;
+                    }
+                    else
+                    {
+                        t[i, j].Next = t[i, j].xNext;
+                        t[i, j].min = t[i, j].xMin;
+                    }
+                }
+            }
+        }
+        private static void Main(string[] args)
+        {
+            int m = 4;
+            int n = 4;
+            string input = "7 9 2 11 13 23 1 3 14 11 20 6 22 44 3 15";
+            string[] items = input.Split(' ');
+            int[,] matrix = new int[m, n];
+            Cell[,] t = new Cell[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = int.Parse(items[i + j*m]);
+                }
+            }
+            Solve(m,n,matrix,t);
+            var p = t[0, 0];
+            Console.Write((p.min+p.value).ToString()+ " ");
+            while (p.Next != null)
+            {
+                Console.Write(p.value+"->");
+                p = (Cell)p.Next;
+            }
+
+            Console.Write(p.value);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Question1/Question2/Readme.txt
@@ -0,0 +1,13 @@
+﻿Given a 2-D MxN matrix having each value as difficulty for the block
+
+Given a 2-D MxN matrix having each value as difficulty for the block. A frog is starting from a point Matrix[0][0] and will have to reach Matrix[M-1][N-1]. It can jump any step in one go [ 1, 2, ..... M-1] horizontally OR [ 1,2,3,.... N-1] vertically Each difficulty value is positive. Write code to give path trace for frog. Two structure to use -
+
+struct node { int x; int y; struct node *next; };
+
+struct path { int difficulty; struct node *pathlink; }
+
+Ex matrix - 4X4 matrix
+
+7 9 2 11 13 23 1 3 14 11 20 6 22 44 3 15
+
+Minimum difficulty = 7 (a[0][0])+ 2(a[0][2]) +3(a[3][2])+15(a[3][3]) = 27 Path trace will have = 7->2->3->15
