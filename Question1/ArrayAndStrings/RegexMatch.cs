399df23fe52a430c681fe368cf066f584fdada16
williamliusea <williamliu_sea@hotmail.com>
2014-06-04 16:07:53 -0700

    fff

---
+++ Question1/ArrayAndStrings/RegexMatch.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ArrayAndStrings
+{
+    public class RegexMatch
+    {
+        public static bool Solution(string s, string r)
+        {
+            if (r.Length == 1)
+            {
+                if (r[0] == s[0] && s.Length == 0)
+                { }
+            }
+            while (true)
+            {
+
+            }
+        }
+    }
+}
